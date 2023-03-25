aef371c8b832134cb36640967d64b9a19d9fae22
Mozammil Khodabacchas <mozammil@procuros.io>
2023-03-25 22:38:45 +0100

    Adjusts snapshot filename generation

--- src/Concerns/SnapshotIdAware.php
+++ src/Concerns/SnapshotIdAware.php
@@ -7,20 +7,50 @@ use ReflectionClass;
 trait SnapshotIdAware
 {
     /*
-     * Determines the snapshot's id. By default, the test case's class and
-     * method names are used.
-     * 
      * Keeping a fork for now until the below PR is merged.
      * 
      * @see https://github.com/spatie/pest-plugin-snapshots/pull/16/files
+     * 
+     * The problem is that even if the PR above is merged, the snapshot files
+     * will still likely be re-generated based on how Pest is normalzing the new
+     * test case names.
      */
     protected function getSnapshotId(): string
     {
-        return sprintf(
-            '%s__%s__%s',
-            (new ReflectionClass($this))->getShortName(),
-            str_replace('__pest_evaluable_', '', str_replace(' ', '_', $this->nameWithDataSet())),
-            $this->snapshotIncrementor
+        $normalizedName = $this->getNormalizedTestName();
+
+        $snapshotId = sprintf(
+            '%s__%s',
+            (new ReflectionClass($this))->getShortName(), 
+            $normalizedName
         );
+
+        if ($this->usesDataProvider()) {
+            $snapshotId .= sprintf(
+                '_with_data_set_%s',
+                $this->getNormalizedDatasetName($this->dataName()),
+            );
+        }
+
+        $snapshotId .= '__' . $this->snapshotIncrementor;
+        
+        return $snapshotId;
+    }
+
+    private function getNormalizedTestName(): string
+    {
+        return str_replace('__pest_evaluable_', '', $this->name());
+    }
+
+    private function getNormalizedDatasetName(string $datasetName): string
+    {
+        $normalizedDatasetName = $datasetName;
+
+        preg_match('/"([^"]+)"/', $datasetName, $matches);
+        if (strlen(trim($matches[1] ?? '')) > 0) {
+            $normalizedDatasetName = $matches[1];
+        }
+
+        return str_replace(' ', '_', $normalizedDatasetName);
     }
 }
