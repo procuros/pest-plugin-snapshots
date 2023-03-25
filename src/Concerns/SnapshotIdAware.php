0f250209134d7f5a63a686d19fdaba8a78890c92
Mozammil Khodabacchas <mozammil@procuros.io>
2023-03-25 18:15:58 +0100

    Adjusts snapshot filename generation

--- src/Concerns/SnapshotIdAware.php
+++ src/Concerns/SnapshotIdAware.php
@@ -9,11 +9,18 @@ trait SnapshotIdAware
     /*
      * Determines the snapshot's id. By default, the test case's class and
      * method names are used.
+     * 
+     * Keeping a fork for now until the below PR is merged.
+     * 
+     * @see https://github.com/spatie/pest-plugin-snapshots/pull/16/files
      */
     protected function getSnapshotId(): string
     {
-        return (new ReflectionClass($this))->getShortName().'__'.
-            str_replace(' ', '_', $this->name()).'__'.
-            $this->snapshotIncrementor;
+        return sprintf(
+            '%s__%s__%s',
+            (new ReflectionClass($this))->getShortName(),
+            str_replace('__pest_evaluable_', '', str_replace(' ', '_', $this->nameWithDataSet())),
+            $this->snapshotIncrementor
+        );
     }
 }
